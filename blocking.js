bc5ab1f4e1cd4cc705ea9dc4bcad6e6636bf6804
Rahul6225 <rahul.thakurbk3000@gmail.com>
2025-07-28 10:54:05 +0530

    Prisma configuration

--- blocking.js
+++ blocking.js
@@ -2,20 +2,20 @@ const crypto = require('crypto');
 const fs = require('fs');
 const https =  require('http');
 
-crypto.pbkdf2("helloPassword","salt",50000,50,"sha512",(err,key)=>{
-    console.log("Key is generated:",key);
+// crypto.pbkdf2("helloPassword","salt",50000,50,"sha512",(err,key)=>{
+//     console.log("Key is generated:",key);
     
-})
-fs.readFileSync("./file.txt", "utf-8") 
-  console.log("file data:");
+// })
+// fs.readFileSync("./file.txt", "utf-8") 
+//   console.log("file data:");
 
 fs.readFile('./file.txt' ,'utf-8',(err,res)=>{
     console.log("file data:",res);
 })
-setImmediate(()=>{
-    console.log("it is setimmediate");
+// setImmediate(()=>{
+//     console.log("it is setimmediate");
     
-})
+// })
 setTimeout(()=>{
     console.log("SetTimeout");
     
