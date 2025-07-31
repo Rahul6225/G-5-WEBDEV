68598f1e51da1c1c67cc52bb0f3eb70939fbdf63
Rahul6225 <rahul.thakurbk3000@gmail.com>
2025-07-31 11:39:45 +0530

    Middleware config

--- express/index.js
+++ express/index.js
@@ -1,23 +1,30 @@
-//Express GENERATOR---> it generate the complete base template for the abcjend with view engine
+//Express GENERATOR---> it generate the complete base template for the backend with view engine
 
 // npx install express-generator
 
 const express = require('express');
 const app = express();
-express.static('./public/');
-app.use(express.static("./public"));
-
-app.use("/static", express.static("public"));
-
 const PORT = 3000
+// express.static('./public/');
+// app.use(express.static("./public"));
 
-// app.use('/',(req,res)=>{
-//     res.end("Hello this is server ");
-// })
+// app.use("/static", express.static("public"));
 
+app.get('/',(req,res)=>{
+    res.end("Hello this is server ");
+})
 app.get('/app',(req,res)=>{
     res.end("neww page");
 })
+ 
+app.use("/",(err,req,res,next)=>{
+    if(err){
+        res.status(500).send("something occured");
+    }
+})
+
 app.listen(PORT,()=>{
     console.log("Server is Running ");
 })
+
+//If ,you need any application level middleware
