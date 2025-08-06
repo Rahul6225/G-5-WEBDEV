5b3d76e49f34e39077c348a261161927199dedf6
Rahul6225 <rahul.thakurbk3000@gmail.com>
2025-08-06 10:42:25 +0530

    Authorization testing

--- Dev_Community/routes/userRoutes.js
+++ Dev_Community/routes/userRoutes.js
@@ -1,6 +1,64 @@
-// const userController = require('../controllers/userController.js');
+const express = require("express");
+const User = require("../models/user.js");
+const userRouter = express.Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-// const express = require('express');
-// const Router = express.Router();
+userRouter.post("/signup", async (req, res) => {
+  try {
+    const { firstName, lastName, emailId, password } = req.body;
+    if (!firstName || !lastName || !emailId || !password) {
+      return res.status(400).send("All fields are required.");
+    }
+    const existingUser = await User.findOne({ emailId });
+    if (existingUser) {
+      return res.status(409).send("User already exists with this email.");
+    }
+    const hashPassword = await bcrypt.hash(password, 10);
+    const user = new User({
+      firstName,
+      lastName,
+      emailId,
+      password: hashPassword,
+    });
 
-// // Router.post("/register",userController);
+    await user.save();
+    res.status(201).send("User added successfully.");
+  } catch (err) {
+    console.error("Signup error:", err);
+    res.status(500).send("Internal server error: " + err.message);
+  }
+});
+
+userRouter.post("/login", async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
+    const user = await User.findOne({ emailId: emailId });
+    if (!emailId || !password) {
+      throw new Error("Enter the credentials!!");
+    }
+    if (!user) {
+      throw new Error("Invalid credentials ");
+    }
+    const ispasword = await bcrypt.compare(password, user.password);
+
+    // res.send(user);
+    if (!ispasword) {
+        res.status(401).send("Invalid password");
+    }
+    const token = await jwt.sign({ _id: user._id }, process.env.SECRET_KEY, {
+      expiresIn: "7d",
+    });
+    res.cookie("token", token);
+    res.send({
+      message: "Great,Login successfully to Dev-Community!!!",
+      user
+    });
+
+    // res.send("Login successfully"+token);
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+});
+
+module.exports = userRouter;
--- Dev_Community/server.js
+++ Dev_Community/server.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const { ConnectDB } = require("./db.js");
-const User = require("./models/user.js");
 const cookieParser = require("cookie-parser");
-const jwt = require("jsonwebtoken");
-const { userAuth } = require("./middlewares/userAuth.js");
+const bcrypt = require("bcrypt");
 
-// const userRoutes = require("./routes/userRoutes");
+
+const { ConnectDB } = require("./config/db.js");
+const userRouter = require("./routes/userRoutes.js");
+
+
 dotenv.config();
 const app = express();
 app.use(express.json());
@@ -18,69 +19,25 @@ const DATABASE_URL =
   process.env.DATABASE_URL || "mongodb://localhost:27017/Dev_Community";
 const DATABASE_NAME = process.env.DATABASE_NAME || "Dev_Community";
 
+app.use("/",userRouter);
+app.use("/",userRouter);
+app.use("/", userRouter);
+
 app.get("/", (req, res) => {
   res.send("HOMEPAGE");
 });
 
-app.post("/signup", async (req, res) => {
-  try {
-    const { firstName, lastName, emailId, password } = req.body;
-    if (!firstName || !lastName || !emailId || !password) {
-      return res.status(400).send("All fields are required.");
-    }
-    const existingUser = await User.findOne({ emailId });
-    if (existingUser) {
-      return res.status(409).send("User already exists with this email.");
-    }
-    const user = new User({
-      firstName,
-      lastName,
-      emailId,
-      password,
-    });
-
-    await user.save();
-    res.status(201).send("User added successfully.");
-  } catch (err) {
-    console.error("Signup error:", err);
-    res.status(500).send("Internal server error: " + err.message);
-  }
-});
-
-app.post("/login", async (req, res) => {
-  try {
-    const { emailId, password } = req.body;
-    const user = await User.findOne({ emailId: emailId, password: password });
-    if (!emailId || !password) {
-      throw new Error("Enter the credentials!!");
-    }
-    if (!user) {
-      throw new Error("Invalid credentials ");
-    }
-    // res.send(user);
-
-    const token = await jwt.sign({ _id: user._id }, process.env.SECRET_KEY, {
-      expiresIn: "7d",
-    });
-    res.cookie("token", token);
-    res.send("Great,Login successfully to Dev-Community!!!");
-
-    // res.send("Login successfully"+token);
-  } catch (err) {
-    res.status(400).send(err.message);
-  }
-});
-app.get("/all-products", (req, res) => {
-  try {
-    const user = req.user;
-    if (!user) {
-     return res.send("It is protected Route");
-    }
-    res.send(user);
-  } catch (error) {
-    res.status(400).send(error.message);
-  }
-});
+// app.get("/all-products", (req, res) => {
+//   try {
+//     const user = req.user;
+//     if (!user) {
+//      return res.send("It is protected Route");
+//     }
+//     res.send(user);
+//   } catch (error) {
+//     res.status(400).send(error.message);
+//   }
+// });
 
 ConnectDB()
   .then(() => {
